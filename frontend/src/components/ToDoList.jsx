7bb35b87f187e82a2a7a6d227054a7ff25481fae
julia <juliaandradesoares@gmail.com>
2022-07-05 10:12:01 -0300

    Adicionando uma nova task ao banco de dados através do front end

--- frontend/src/components/ToDoList.jsx
+++ frontend/src/components/ToDoList.jsx
@@ -11,12 +11,30 @@ class ToDoList extends React.Component {
     };
 
     this.onChangeNewTask = this.onChangeNewTask.bind(this);
-    // this.onClickNewTask = this.onClickNewTask.bind(this);
+    this.onClickNewTask = this.onClickNewTask.bind(this);
     // this.onClickDeleteTask = this.onClickDeleteTask.bind(this);
   }
 
   async componentDidMount() {
 
+    const fetchTasks = async () => {
+      try {
+        const response = await fetch(urlFetch);
+        const data = await response.json();
+        return data;
+      } catch (erro) {
+        return new Error('You must provide an url');
+      }
+    };
+    
+    const tasks = await fetchTasks();
+  
+    this.setState({
+      tasks,
+    })
+  }
+
+  async componentDidUpdate() {
     const fetchTasks = async () => {
       try {
         const response = await fetch(urlFetch);
@@ -40,11 +58,23 @@ class ToDoList extends React.Component {
     });
   }
 
-  /* onClickNewTask() {
+  // fonte: https://www.freecodecamp.org/portuguese/news/tutorial-de-fetch-api-em-javascript-exemplos-de-post-e-cabecalho/
+  onClickNewTask() {
     const { newTask } = this.state;
 
     // CHAMAR POST PASSANDO NEWTASK
-  } */
+
+    let task = {
+      task: newTask,
+    }
+    
+    fetch(urlFetch, {
+      method: "POST",
+      body: JSON.stringify(task),
+      headers: {"Content-type": "application/json; charset=UTF-8"}
+    })
+    .then(response => response.json());
+  }
 
   /* onClickDeleteTask(id) {
     // CHAMAR DELETE PASSANDO ID
@@ -52,7 +82,6 @@ class ToDoList extends React.Component {
 
   render() {
     const { newTask, tasks } = this.state;
-    console.log(tasks)
     return (
       <>
         <div>
@@ -67,7 +96,7 @@ class ToDoList extends React.Component {
           </label>
           <button
             type='button'
-            // onClick={ this.onClickNewTask }
+            onClick={ this.onClickNewTask }
           >
             Adicionar tarefa
           </button>
